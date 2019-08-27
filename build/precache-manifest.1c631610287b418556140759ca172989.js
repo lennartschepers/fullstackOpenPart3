72034d42c382dce8bd0152f5786e1517e1239015
lennartschepers <lenschepers@github.com>
2019-08-27 17:11:11 +0200

    uibuild

---
+++ build/precache-manifest.1c631610287b418556140759ca172989.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfddb4263e70c0749f579f51e930a8d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "39ba76e626e292c9acdb",
+    "url": "/static/js/main.64c27a29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(express.static('build'))
 app.use(cors())
 
 let persons = [
